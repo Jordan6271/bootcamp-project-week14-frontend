ff8530c80c4e531adf087e50f0634572bc473426
Jordan6271 <jordan.robertson-browne@mattressonline.co.uk>
2021-05-10 13:20:33 +0100

    Change url to point to heroku deployment

--- eventful/src/Eventful/ApiClient/ApiClient.js
+++ eventful/src/Eventful/ApiClient/ApiClient.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const url = "http://localhost:3001/";
+const url = "https://bootcamp-week14-eventful.herokuapp.com/";
 
 export class ApiClient {
     constructor(tokenProvider, logoutHandler) {
