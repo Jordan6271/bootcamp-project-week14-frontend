32b9970eaad9bc63ff979fba37a07ea0d8bc6d21
Jordan6271 <jordan.robertson-browne@mattressonline.co.uk>
2021-05-13 20:37:04 +0100

    Style login screen

--- eventful/src/Eventful/Eventful.js
+++ eventful/src/Eventful/Eventful.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 import { ApiClient } from "./ApiClient/ApiClient";
 import Dashboard from "./Dashboard/Dashboard";
 import Login from "./Login/Login";
