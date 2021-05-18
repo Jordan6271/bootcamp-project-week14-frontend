0b1a7146c2eef96208395660f9e4f68e57fc7b4f
Jordan6271 <jordan.robertson-browne@mattressonline.co.uk>
2021-05-18 15:44:13 +0100

    Add navbar brand to login page

--- eventful/src/Eventful/Login/Login.js
+++ eventful/src/Eventful/Login/Login.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Navbar from "react-bootstrap/Navbar";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 
@@ -34,29 +35,34 @@ class Login extends React.Component {
 
     render() {
         return (
-            <div className="w-25 m-auto text-center pt-5">
-                <Form onSubmit={(event) => this.submitHandler(event)}>
-                    <h1>Log In</h1>
-                    <Form.Group controlId="formUsername" className="pt-4">
-                        <Form.Label>Username:</Form.Label>
-                        <Form.Control
-                            type="username"
-                            placeholder="Enter username"
-                            required
-                        />
-                    </Form.Group>
-                    <Form.Group controlId="formPassword" className="py-4">
-                        <Form.Label>Password:</Form.Label>
-                        <Form.Control
-                            type="password"
-                            placeholder="Enter password"
-                            required
-                        />
-                    </Form.Group>
-                    <Button variant="primary" type="submit">
-                        Submit
-                    </Button>
-                </Form>
+            <div>
+                <Navbar bg="dark" variant="dark">
+                    <Navbar.Brand className="mx-5">Eventful</Navbar.Brand>
+                </Navbar>
+                <div className="w-25 m-auto text-center pt-4">
+                    <Form onSubmit={(event) => this.submitHandler(event)}>
+                        <h1>Log In</h1>
+                        <Form.Group controlId="formUsername" className="pt-4">
+                            <Form.Label>Username:</Form.Label>
+                            <Form.Control
+                                type="username"
+                                placeholder="Enter username"
+                                required
+                            />
+                        </Form.Group>
+                        <Form.Group controlId="formPassword" className="py-4">
+                            <Form.Label>Password:</Form.Label>
+                            <Form.Control
+                                type="password"
+                                placeholder="Enter password"
+                                required
+                            />
+                        </Form.Group>
+                        <Button variant="primary" type="submit">
+                            Submit
+                        </Button>
+                    </Form>
+                </div>
             </div>
         );
     }
