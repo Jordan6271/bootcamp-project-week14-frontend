838e7f8e77d82ed1a7fad91eb0a95f90f36dd5e9
Jordan6271 <jordan.robertson-browne@mattressonline.co.uk>
2021-05-18 17:53:13 +0100

    Convert to functional components

--- eventful/src/Eventful/Dashboard/Dashboard.js
+++ eventful/src/Eventful/Dashboard/Dashboard.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Dashboard.css";
 import Add from "../Add/Add";
 
@@ -9,39 +9,28 @@ import Table from "react-bootstrap/Table";
 
 import { HashRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-class Dashboard extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            events: [],
-            currentEvent: undefined,
-        };
-    }
+const Dashboard = (props) => {
+    const [events, setEvents] = useState([]);
+    const [currentEvent, setCurrentEvent] = useState(undefined);
 
-    refreshList() {
-        this.props.apiClient.getEvent().then((response) =>
-            this.setState({
-                events: response.data,
-            })
-        );
-    }
+    const refreshList = () => {
+        props.apiClient.getEvent().then((response) => setEvents(response.data));
+    };
 
-    removeEvent(id) {
-        this.props.apiClient.removeEvent(id).then(() => this.refreshList());
-    }
+    const removeEvent = (id) => {
+        props.apiClient.removeEvent(id).then(() => refreshList());
+    };
 
-    updateEvent(event) {
-        this.setState({
-            currentEvent: event,
-        });
-    }
+    const updateEvent = (event) => {
+        setCurrentEvent(event);
+    };
 
-    componentDidMount() {
-        this.refreshList();
-    }
+    useEffect(() => {
+        refreshList();
+    }, []);
 
-    buildRows() {
-        return this.state.events.map((current) => {
+    const buildRows = () => {
+        return events.map((current) => {
             return (
                 <tr key={current._id}>
                     <td className="table-width-15">{current.name}</td>
@@ -53,14 +42,14 @@ class Dashboard extends React.Component {
                         <Link to="/add" className="text-decoration-none">
                             <span
                                 className="action-link update mx-2"
-                                onClick={() => this.updateEvent(current)}
+                                onClick={() => updateEvent(current)}
                             >
                                 Update
                             </span>
                         </Link>
                         <span
                             className="action-link remove mx-2"
-                            onClick={() => this.removeEvent(current._id)}
+                            onClick={() => removeEvent(current._id)}
                         >
                             Remove
                         </span>
@@ -68,90 +57,76 @@ class Dashboard extends React.Component {
                 </tr>
             );
         });
-    }
+    };
 
-    render() {
-        return (
-            <Router>
-                <Navbar bg="dark" variant="dark">
-                    <Navbar.Brand className="mx-5">Eventful</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav>
-                            <Link to="/" className="nav-link text-danger px-3">
-                                View Events
-                            </Link>
-                            <Link
-                                to="/add"
-                                className="nav-link text-danger px-3"
-                            >
-                                Add New Event
-                            </Link>
-                        </Nav>
-                        <Nav className="m-auto" />
-                        <Nav>
-                            <Link
-                                to="/"
-                                className="ml-auto nav-link text-danger px-5"
-                                onClick={() =>
-                                    this.props.apiClient.logoutHandler()
-                                }
-                            >
-                                Logout
-                            </Link>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Navbar>
-                <Container>
-                    <Switch>
-                        <Route path="/add">
-                            <Add
-                                apiClient={this.props.apiClient}
-                                refreshList={() => {
-                                    this.refreshList();
-                                    this.setState({
-                                        currentEvent: undefined,
-                                    });
-                                }}
-                                currentEvent={this.state.currentEvent}
-                            />
-                        </Route>
-                        <Route exact path="/">
-                            <div className="mx-auto text-center pt-4">
-                                <h1>My Events</h1>
-                                {this.state.events.length ? (
-                                    <Table
-                                        striped
-                                        bordered
-                                        hover
-                                        id="events-table"
-                                    >
-                                        <thead>
-                                            <tr>
-                                                <th>Event Name</th>
-                                                <th>Location</th>
-                                                <th>Description</th>
-                                                <th>Date</th>
-                                                <th>Time</th>
-                                                <th>Actions</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>{this.buildRows()}</tbody>
-                                    </Table>
-                                ) : (
-                                    <div className="mt-4">
-                                        Sorry, you have no events yet.{" "}
-                                        <Link to="/add">Try adding one!</Link>
-                                    </div>
-                                )}
-                            </div>
-                        </Route>
-                        <Route path="/">Error: 404 not found</Route>
-                    </Switch>
-                </Container>
-            </Router>
-        );
-    }
-}
+    return (
+        <Router>
+            <Navbar bg="dark" variant="dark">
+                <Navbar.Brand className="mx-5">Eventful</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav>
+                        <Link to="/" className="nav-link text-danger px-3">
+                            View Events
+                        </Link>
+                        <Link to="/add" className="nav-link text-danger px-3">
+                            Add New Event
+                        </Link>
+                    </Nav>
+                    <Nav className="m-auto" />
+                    <Nav>
+                        <Link
+                            to="/"
+                            className="ml-auto nav-link text-danger px-5"
+                            onClick={() => props.apiClient.logoutHandler()}
+                        >
+                            Logout
+                        </Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+            <Container>
+                <Switch>
+                    <Route path="/add">
+                        <Add
+                            apiClient={props.apiClient}
+                            refreshList={() => {
+                                refreshList();
+                                setCurrentEvent(undefined);
+                            }}
+                            currentEvent={currentEvent}
+                        />
+                    </Route>
+                    <Route exact path="/">
+                        <div className="mx-auto text-center pt-4">
+                            <h1>My Events</h1>
+                            {events.length ? (
+                                <Table striped bordered hover id="events-table">
+                                    <thead>
+                                        <tr>
+                                            <th>Event Name</th>
+                                            <th>Location</th>
+                                            <th>Description</th>
+                                            <th>Date</th>
+                                            <th>Time</th>
+                                            <th>Actions</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>{buildRows()}</tbody>
+                                </Table>
+                            ) : (
+                                <div className="mt-4">
+                                    Sorry, you have no events yet.{" "}
+                                    <Link to="/add">Try adding one!</Link>
+                                </div>
+                            )}
+                        </div>
+                    </Route>
+                    <Route path="/">Error: 404 not found</Route>
+                </Switch>
+            </Container>
+        </Router>
+    );
+};
 
 export default Dashboard;
--- eventful/src/Eventful/Login/Login.js
+++ eventful/src/Eventful/Login/Login.js
@@ -1,27 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 
-class Login extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            disabled: false,
-        };
-    }
+const Login = (props) => {
+    const [disabled, setDisabled] = useState(false);
 
-    submitHandler(event) {
+    const submitHandler = (event) => {
         event.preventDefault();
-        this.setState({ disabled: true });
-        this.props.apiClient
+        setDisabled(true);
+        props.apiClient
             .login(
                 event.target.formUsername.value,
                 event.target.formPassword.value
             )
             .then((response) => {
-                this.setState({ disabled: false });
-                this.props.loggedIn(response.data.token);
+                setDisabled(false);
+                props.loggedIn(response.data.token);
             })
             .catch((error) => {
                 if (error.response.status === 401) {
@@ -29,43 +24,41 @@ class Login extends React.Component {
                 } else {
                     alert(error);
                 }
-                this.setState({ disabled: false });
+                setDisabled(false);
             });
-    }
+    };
 
-    render() {
-        return (
-            <div>
-                <Navbar bg="dark" variant="dark">
-                    <Navbar.Brand className="mx-5">Eventful</Navbar.Brand>
-                </Navbar>
-                <div className="w-25 m-auto text-center pt-4">
-                    <Form onSubmit={(event) => this.submitHandler(event)}>
-                        <h1>Log In</h1>
-                        <Form.Group controlId="formUsername" className="pt-4">
-                            <Form.Label>Username:</Form.Label>
-                            <Form.Control
-                                type="username"
-                                placeholder="Enter username"
-                                required
-                            />
-                        </Form.Group>
-                        <Form.Group controlId="formPassword" className="py-4">
-                            <Form.Label>Password:</Form.Label>
-                            <Form.Control
-                                type="password"
-                                placeholder="Enter password"
-                                required
-                            />
-                        </Form.Group>
-                        <Button variant="primary" type="submit">
-                            Submit
-                        </Button>
-                    </Form>
-                </div>
+    return (
+        <div>
+            <Navbar bg="dark" variant="dark">
+                <Navbar.Brand className="mx-5">Eventful</Navbar.Brand>
+            </Navbar>
+            <div className="w-25 m-auto text-center pt-4">
+                <Form onSubmit={(event) => submitHandler(event)}>
+                    <h1>Log In</h1>
+                    <Form.Group controlId="formUsername" className="pt-4">
+                        <Form.Label>Username:</Form.Label>
+                        <Form.Control
+                            type="username"
+                            placeholder="Enter username"
+                            required
+                        />
+                    </Form.Group>
+                    <Form.Group controlId="formPassword" className="py-4">
+                        <Form.Label>Password:</Form.Label>
+                        <Form.Control
+                            type="password"
+                            placeholder="Enter password"
+                            required
+                        />
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                        Submit
+                    </Button>
+                </Form>
             </div>
-        );
-    }
-}
+        </div>
+    );
+};
 
 export default Login;
